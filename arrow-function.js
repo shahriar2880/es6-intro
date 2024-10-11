acc755c1a7cece7efb8538987d347f8dca8d4a5b
shahriar2880 <shahriarreza436@gmail.com>
2024-10-11 21:13:38 +0600

    single parameter or one parameter

--- arrow-function.js
+++ arrow-function.js
@@ -18,3 +18,13 @@ console.log(sum2);
 const add3 = (a,b) => a+b ;
 const sum3 = add3(9,4);
 console.log(sum3);
+
+//single parameter or one parameter
+const getAge = (person) => person.age ;
+const student = {
+    name: "shahriar reza",
+    age: 26,
+    desig: "Job Holder"
+}
+const age = getAge(student);
+console.log(age);
