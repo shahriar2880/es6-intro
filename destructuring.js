19872afb48cd705bbdb331bc6a80b67eca12f508
shahriar2880 <shahriarreza436@gmail.com>
2024-10-12 00:12:04 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,11 @@
+const student = {
+    name: "shahriar",
+    age: 26,
+    gender: "male"
+}
+
+const {name, age, gender} = student;
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.gender);
