35472bd6d26265f9cd979759f7561f8b7f83fb59
shahriar2880 <shahriarreza436@gmail.com>
2024-10-13 17:17:55 +0600

    chain

---
+++ chain.js
@@ -0,0 +1,10 @@
+const products = {
+    count: 5000,
+    data: [
+        {id: 1, name: 'shahriar reza', section: 'B',GPA: 3.46},
+        {id: 2, name: 'Rony kumar', section: 'B',GPA: 3.4},
+        {id: 3, name: 'Moniruzzaman', section: 'B',GPA: 3.06}
+    ]
+}
+
+console.log(products.data[0].name)
