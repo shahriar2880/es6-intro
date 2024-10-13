93b7f9933218b0f4107c581d99bd1cba37298832
shahriar2880 <shahriarreza436@gmail.com>
2024-10-13 17:37:28 +0600

    chain

--- chain.js
+++ chain.js
@@ -1,10 +1,27 @@
-const products = {
-    count: 5000,
-    data: [
-        {id: 1, name: 'shahriar reza', section: 'B',GPA: 3.46},
-        {id: 2, name: 'Rony kumar', section: 'B',GPA: 3.4},
-        {id: 3, name: 'Moniruzzaman', section: 'B',GPA: 3.06}
-    ]
-}
+const students = {
+  count: 5000,
+  data: [
+    { id: 1, name: "shahriar reza", section: "B", GPA: 3.46 },
+    { id: 2, name: "Rony kumar", section: "B", GPA: 3.4 },
+    { id: 3, name: "Moniruzzaman", section: "B", GPA: 3.06 },
+  ],
+};
 
-console.log(products.data[0].name)
+console.log(students.data[0].name);
+
+const user = {
+  id: 1,
+  name: "shahriar reza",
+  section: "B",
+  GPA: 3.46,
+  Address: {
+    street: {
+        first: '54/1 2b',
+        second: 'Bashbari Mohammadpur',
+        third: 'sghbg'
+    },
+    city: 'Dhaka'
+  }
+};
+
+console.log(user.Address.street.first)
